44a0b84fbf5d048f5b81fd80ef4800a1cfcd7c78
aadinathdeepak <aadinathdeepak10@gmail.com>
2025-06-01 13:06:08 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="" />
+          <h1>
+            Discover <span className="text-gradient">Movies</span> <br />
+            Like Never Before
+          </h1>
+        </header>
+      </div>
+    </main>
+  );
+};
+
+export default App;
