6609fd10722eeaece95f3dbe8fd1c84688176991
aadinathdeepak <aadinathdeepak10@gmail.com>
2025-06-12 17:48:04 +0530

    added loader animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Search from "./components/Search";
+import Loader from "./components/Loader";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -18,7 +19,7 @@ const App = () => {
 
   const [movieList, setMovieList] = useState([]);
 
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMovies = async () => {
     try {
@@ -43,7 +44,7 @@ const App = () => {
       setMovieList([]);
       return;
     } finally {
-      setIsLoading(true);
+      setIsLoading(false);
     }
   };
   useEffect(() => {
@@ -68,9 +69,9 @@ const App = () => {
           ></Search>
         </header>
         <section className="all-movies">
-          <h2>All Movies</h2>
+          <h2 className="mt-[40px] text-center">All Movies</h2>
           {isLoading ? (
-            <p className="text-white">Loading movies...</p>
+            <Loader></Loader>
           ) : errorMessage ? (
             <p className="text-red-500">{errorMessage}</p>
           ) : (
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,109 +4,67 @@ import styled from "styled-components";
 const Loader = () => {
   return (
     <StyledWrapper>
-      <div className="card">
-        <div className="loader">
-          <p>loading</p>
-          <div className="words">
-            <span className="word">buttons</span>
-            <span className="word">forms</span>
-            <span className="word">switches</span>
-            <span className="word">cards</span>
-            <span className="word">buttons</span>
-          </div>
-        </div>
+      <div>
+        <svg height={0} width={0}>
+          <defs>
+            <filter
+              colorInterpolationFilters="sRGB"
+              height="200%"
+              y="-50%"
+              width="200%"
+              x="-50%"
+              id="goo"
+            >
+              <feGaussianBlur
+                result="blur"
+                stdDeviation={8}
+                in="SourceGraphic"
+              />
+              <feColorMatrix
+                result="cm"
+                values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 21 -7"
+                mode="matrix"
+                in="blur"
+              />
+            </filter>
+          </defs>
+        </svg>
+        <svg height={180} width={320} viewBox="0 0 320 180">
+          <g filter="url(#goo)">
+            <circle cy={90} cx={160} r={24} fill="#275EFE" className="circle" />
+            <circle
+              cy={90}
+              cx={160}
+              r={24}
+              fill="#275EFE"
+              className="circle right"
+            />
+          </g>
+        </svg>
       </div>
     </StyledWrapper>
   );
 };
 
 const StyledWrapper = styled.div`
-  .card {
-    /* color used to softly clip top and bottom of the .words container */
-    --bg-color: #111;
-    background-color: var(--bg-color);
-    padding: 1rem 2rem;
-    border-radius: 1.25rem;
-  }
-  .loader {
-    color: rgb(124, 124, 124);
-    font-family: "Poppins", sans-serif;
-    font-weight: 500;
-    font-size: 25px;
-    -webkit-box-sizing: content-box;
-    box-sizing: content-box;
-    height: 40px;
-    padding: 10px 10px;
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .circle {
+    animation: move571 4s linear infinite;
   }
 
-  .words {
-    overflow: hidden;
-    position: relative;
-  }
-  .words::after {
-    content: "";
-    position: absolute;
-    inset: 0;
-    background: linear-gradient(
-      var(--bg-color) 10%,
-      transparent 30%,
-      transparent 70%,
-      var(--bg-color) 90%
-    );
-    z-index: 20;
+  .circle.right {
+    animation-direction: reverse;
   }
 
-  .word {
-    display: block;
-    height: 100%;
-    padding-left: 6px;
-    color: #956afa;
-    animation: spin_4991 4s infinite;
-  }
-
-  @keyframes spin_4991 {
-    10% {
-      -webkit-transform: translateY(-102%);
-      transform: translateY(-102%);
-    }
-
+  @keyframes move571 {
     25% {
-      -webkit-transform: translateY(-100%);
-      transform: translateY(-100%);
-    }
-
-    35% {
-      -webkit-transform: translateY(-202%);
-      transform: translateY(-202%);
-    }
-
-    50% {
-      -webkit-transform: translateY(-200%);
-      transform: translateY(-200%);
-    }
-
-    60% {
-      -webkit-transform: translateY(-302%);
-      transform: translateY(-302%);
+      transform: translateX(-32px);
     }
 
     75% {
-      -webkit-transform: translateY(-300%);
-      transform: translateY(-300%);
-    }
-
-    85% {
-      -webkit-transform: translateY(-402%);
-      transform: translateY(-402%);
-    }
-
-    100% {
-      -webkit-transform: translateY(-400%);
-      transform: translateY(-400%);
+      transform: translateX(32px);
     }
   }
 `;
